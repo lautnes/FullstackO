42cf3331340b0caca683ab772cc6ca07499da5e1
Nestori Lautanala <nestori.lautanala@gmail.com>
2024-07-18 16:37:02 +0300

    Add Osa1 Ex3 and Ex4 projects

---
+++ Osa1/Ex3/part3/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/Header';
+import Content from './components/Content';
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} />
+    </div>
+  );
+}
+
+export default App;
