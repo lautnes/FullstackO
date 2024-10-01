b9a3c3987a8b94c401a58c2c6ac619b8722ad468
Nestori Lautanala <nestori.lautanala@gmail.com>
2024-10-01 18:58:54 +0300

    Ex12 completed

--- Osa3/EX12/mongo.js
+++ Osa3/EX12/mongo.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
-}
+// Lataa .env-tiedoston arvot
+dotenv.config();
 
-const password = process.argv[2];
-
-const url = `mongodb+srv://fullstack:${password}@cluster0.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+const url = process.env.MONGODB_URI;
 
 mongoose.set('strictQuery', false);
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(url);
+
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -19,7 +17,17 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema);
 
-if (process.argv.length === 5) {
+if (process.argv.length === 3) {
+
+  Person.find({}).then(result => {
+    console.log('phonebook:');
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+
   const name = process.argv[3];
   const number = process.argv[4];
 
@@ -28,18 +36,11 @@ if (process.argv.length === 5) {
     number: number,
   });
 
-  person.save().then((result) => {
+  person.save().then(result => {
     console.log(`added ${name} number ${number} to phonebook`);
     mongoose.connection.close();
   });
-}
-
-else if (process.argv.length === 3) {
-  Person.find({}).then((result) => {
-    console.log('phonebook:');
-    result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
+} else {
+  console.log('Please provide the correct number of arguments: node mongo.js <password> <name> <number>');
+  process.exit(1);
 }
